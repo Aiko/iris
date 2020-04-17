9f6b47b7fa2579a05c42737ac3744f12678f859d
Priansh Shah <shah@priansh.com>
2020-04-16 23:28:19 -0400

    uh oh fuckeroo!

--- src/public/assets/js/services/mail.js
+++ src/public/assets/js/services/mail.js
@@ -219,6 +219,7 @@ const mailapi = {
             info(...MAILAPI_TAG, "Fetching latest 100 emails from inbox.")
             const emails = await this.callIPC(
                 this.task_FetchEmails("INBOX", `${uidMin}:${uidNext}`, false))
+            if (!emails) return error(...MAILAPI_TAG, emails)
             const processed_emails = emails
                 .reverse()
                 .map(email => {
