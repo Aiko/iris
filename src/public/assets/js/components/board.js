4a4d5c342d8ad78890dadfad43a4eab165d1bd3e
Priansh Shah <shah@priansh.com>
2020-06-20 23:57:38 -0400

    whats good holmes

--- src/public/assets/js/components/board.js
+++ src/public/assets/js/components/board.js
@@ -1,5 +1,10 @@
 Vue.component('board', {
   props: ['boardName', 'board', 'syncing'],
+  data() {
+    return {
+      thin: false
+    }
+  },
   computed: {
     prettyBoardName () {
       return this.boardName.replace('[Aiko Mail]/', '')
