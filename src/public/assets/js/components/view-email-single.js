602a4139b50a6a4b6d8c6640861b311681d2f280
Priansh Shah <shah@priansh.com>
2020-06-21 03:17:29 -0400

    fastest

--- src/public/assets/js/components/view-email-single.js
+++ src/public/assets/js/components/view-email-single.js
@@ -40,7 +40,7 @@ Vue.component('view-email-single', {
       const iframeID = this.iframeId
       const el = document.getElementById(iframeID)
       if (el) el.style.height = '0px'
-      else setTimeout(this.setContent, 100) // try every 100ms
+      else return setTimeout(this.setContent, 100) // try every 100ms
       $('.email-body').scrollTop(0)
       const doc = el.contentWindow.document
       doc.open()
@@ -53,12 +53,9 @@ Vue.component('view-email-single', {
       try {
         $('#' + iframeID).load(function () {
           document.getElementById(iframeID).style.height = Math.max(document.getElementById(iframeID).contentWindow.document.body.offsetHeight, document.getElementById(iframeID).contentWindow.document.body.scrollHeight) + 'px'
-          $('.email-body').scrollTop(0)
         })
       } catch (e) { }
-      /*
       document.getElementById(iframeID).style.height = Math.max(document.getElementById(iframeID).contentWindow.document.body.offsetHeight, document.getElementById(iframeID).contentWindow.document.body.scrollHeight) + 'px'
-      $('.email-body').scrollTop(0)
       // Don't judge me. It works.
       setTimeout(function () {
         document.getElementById(iframeID).style.height = Math.max(document.getElementById(iframeID).contentWindow.document.body.offsetHeight, document.getElementById(iframeID).contentWindow.document.body.scrollHeight) + 'px'
@@ -69,7 +66,6 @@ Vue.component('view-email-single', {
       setTimeout(function () {
         document.getElementById(iframeID).style.height = Math.max(document.getElementById(iframeID).contentWindow.document.body.offsetHeight, document.getElementById(iframeID).contentWindow.document.body.scrollHeight) + 'px'
       }, 500)
-      */
       const links = doc.links
       for (let i = 0; i < links.length; i++) {
         links[i].target = '_blank'
