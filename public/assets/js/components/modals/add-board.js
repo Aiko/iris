f60f29553ed85887ebbd98458a881fa786042701
Priansh Shah <shah@priansh.com>
2021-01-23 04:52:01 +0000

    modernify add board modal

--- public/assets/js/components/modals/add-board.js
+++ public/assets/js/components/modals/add-board.js
@@ -12,26 +12,12 @@ Vue.component('add-board-modal', {
   },
   methods: {
     async addBoard () {
-      // TODO: check to make sure name is valid
       info(...MODALS_TAG, 'Making a new board with title:', this.name)
-      const path = app.folderWithSlug(this.name)
-      const s = await app.callIPC(app.task_NewFolder(path))
-      if (!s || s.error) { return window.error(...MODALS_TAG, "Couldn't make board:", s?.error) }
-      info(...MODALS_TAG, 'Made a new board with path:', path)
-      app.boardNames.push(path)
-      Vue.set(app.boards, path, {
-        uidLatest: -1,
-        emails: []
-        // modSeq: -1,
-      })
-      info(...MODALS_TAG, "Updated the app's view model.")
-      await BigStorage.store(app.imapConfig.email + '/boards', app.boards)
-      await SmallStorage.store(app.imapConfig.email + ':board-names', app.boardNames)
-      info(...MODALS_TAG, 'Saved the boards cache for the app.')
+      await this.$root.boardCreate(this.name)
       this.close()
     },
     async close () {
-      app.addBoard = false
+      this.$root.addBoard = false
     }
   }
 })
