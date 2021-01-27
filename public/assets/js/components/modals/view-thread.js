915690f83a4f43980c736b196b6cb20bca142c18
Priansh Shah <shah@priansh.com>
2021-01-27 04:36:44 +0000

    bugfixes to view-thread

--- public/assets/js/components/modals/view-thread.js
+++ public/assets/js/components/modals/view-thread.js
@@ -4,6 +4,7 @@ Vue.component('view-thread', {
     return {
       avatar: 'assets/img/avatar.png',
       fullThread: this.thread,
+      email: this.thread.emails[0]
     }
   },
   async created () {
@@ -16,12 +17,25 @@ Vue.component('view-thread', {
     })
 
     //? fetch avatar
-    this.avatar = await email.M.envelope.from.address.getAvatar() //* in utils.js we added this to string proto
+    this.email = ((thread, sentFolder) => {
+      for (const email of thread.emails) {
+        const sentLoc = email.locations.filter(({ folder }) => folder == sentFolder)?.[0]
+        if (!sentLoc) return email
+      }
+      return thread.emails?.[0]
+    })(this.thread, this.$root.folders.sent)
+
+    this.avatar = await this.email.M.envelope.from.address.getAvatar() //* in utils.js we added this to string proto
 
     //? fetch the full thread
     this.fullThread = await this.$root.engine.api.get.thread(this.thread.tid)
-    const email = this.thread.emails.filter(
-      e => !(e.locations.filter(({ folder }) => folder == sentFolder)?.[0]))?.[0] || this.thread.emails[0]
+    this.email = ((thread, sentFolder) => {
+      for (const email of thread.emails) {
+        const sentLoc = email.locations.filter(({ folder }) => folder == sentFolder)?.[0]
+        if (!sentLoc) return email
+      }
+      return thread.emails?.[0]
+    })(this.fullThread, this.$root.folders.sent)
   },
   methods: {
     close () {
