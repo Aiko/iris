fe1eee94d695ff9630635b83e029f74166b7d021
Ruben <rubencharlestouitou@gmail.com>
2021-02-22 23:15:41 +0100

    overlay wont work........

--- public/assets/js/components/tour.js
+++ public/assets/js/components/tour.js
@@ -1,15 +1,31 @@
-import Shepherd from '../third-party/shepherd.min.js';
-
 const tour = new Shepherd.Tour({
   defaultStepOptions: {
     classes: 'shadow-md bg-purple-dark',
-    scrollTo: true
+    scrollTo: true,
+    useModalOverlay: true
   }
 });
 
 tour.addStep({
-  id: 'example-step',
-  text: 'This step is attached to the bottom of the <code>.example-css-selector</code> element.',
+  id: 'step1',
+  text: 'This is your inbox, all your emails are sorted as <b>Priority</b> and <b>Other</b>',
+  attachTo: {
+    element: '.inbox',
+    on: 'bottom'
+  },
+  
+  classes: 'example-step-extra-class',
+  buttons: [
+    {
+      text: 'Next',
+      action: tour.next
+    }
+  ]
+});
+
+tour.addStep({
+  id: 'step2',
+  text: 'This step 2',
   attachTo: {
     element: '.example-css-selector',
     on: 'bottom'
@@ -22,5 +38,6 @@ tour.addStep({
     }
   ]
 });
+
 
 tour.start();
