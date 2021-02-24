ede0a6283c5a858a8f6e4530ce6222b2c8a42fc8
Priansh Shah <shah@priansh.com>
2021-02-24 19:00:26 +0000

    g6

--- Mouseion/src/cleaner.js
+++ Mouseion/src/cleaner.js
@@ -259,8 +259,8 @@ const Janitor = (async (Lumberjack, folder, useAiko=false) => {
         //* in the special case of confirm_code, look for the confirmation code
         if (result.intent == 'confirm_code' && !email.M.quick_actions.otp) {
           let code = result.entities.filter(({ subtype }) => subtype == "code")?.[0]?.value
-          if (!code) code = /[0-9]{5,7}/g.exec(email.parsed.text)?.[0]
-          if (!code) code = /\b[0-9A-Z]{5,7}\b/g.exec(email.parsed.text)?.[0]
+          // if (!code) code = /[0-9]{5,7}/g.exec(email.parsed.text)?.[0]
+          if (!code) code = /\b[0-9A-Z]{5,12}\b/g.exec(email.parsed.text)?.[0]
           if (code) email.M.quick_actions.otp = code
         }
       }
