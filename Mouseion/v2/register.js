8535f5a1290015747c4e253d167fc2330f06e51b
Priansh Shah <shah@priansh.com>
2021-06-22 19:30:23 -0400

    print error when loading unregistered modules

---
+++ Mouseion/v2/register.js
@@ -0,0 +1,8 @@
+require('colors')
+
+module.exports = () => {
+  const defs = {}
+
+  const register = (key, def) => defs[key] = def
+  const get = key => defs[key] || console.error(`Attempted to load ${key} module, but it has not been registered.`.red)
+}
