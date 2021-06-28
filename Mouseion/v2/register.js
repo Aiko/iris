cf15dc631ef21984b98f0349fce083759f40efb9
Priansh Shah <shah@priansh.com>
2021-06-28 19:17:59 -0400

    double wink operator ;;

--- Mouseion/v2/register.js
+++ Mouseion/v2/register.js
@@ -3,6 +3,6 @@ require('colors')
 module.exports = () => {
   const defs = {}
 
-  const register = (key, def) => defs[key] = def
-  const get = key => defs[key] || console.error(`Attempted to load ${key} module, but it has not been registered.`.red)
+  const register = (key, def) => defs[key] = def ;;
+  const get = key => defs[key] || console.error(`Attempted to load ${key} module, but it has not been registered.`.red) ;;
 }
