1dc370b6a7fdcc05e7f913c77d94f7937788de45
Priansh Shah <shah@priansh.com>
2021-07-27 01:30:20 -0400

    this disgusting eyesore is do-in-batch in ts

---
+++ Mouseion/utils/do-in-batch.ts
@@ -0,0 +1,27 @@
+export default async function <A, B>(
+  elements: A[], batch_size: number,
+  consumer: (_: A) => Promise<B>, monitor: () => void | Promise<void> = ()=>{},
+  verbose: boolean = false
+): Promise<B[]> {
+  let promises: Promise<B>[] = []
+  const results: B[] = []
+
+  let i = 0
+  for (const element of elements) {
+
+    //? consume batch if we have reached the maximum size
+    if (promises.length >= batch_size) {
+      results.push(...(await Promise.all(promises)))
+      promises = []
+      i++
+      if (verbose) console.log("Processed", i*batch_size, "of", elements.length)
+      await monitor()
+    }
+    promises.push(consumer(element))
+  }
+
+  //? consume any remainders
+  results.push(...(await Promise.all(promises)))
+  if (verbose) console.log("Processed", elements.length)
+  return results
+}
