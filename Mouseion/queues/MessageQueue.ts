0667ed895e16c3b3b0143e4f7f51432a424eda60
Priansh Shah <shah@priansh.com>
2021-08-10 18:48:53 -0400

    lol, i meant promise<boolean> not promise<true>

--- Mouseion/queues/MessageQueue.ts
+++ Mouseion/queues/MessageQueue.ts
@@ -2,6 +2,6 @@ import { MessageID } from "../post-office/types";
 
 export default interface MessageQueue {
   readonly pending: MessageID[]
-  consume(): Promise<true>
+  consume(): Promise<boolean>
   queue(mids: MessageID[]): void
 }
