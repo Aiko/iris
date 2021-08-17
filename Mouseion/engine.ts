f1a69768f6ad552dd0a0654cdae6789afb5af4bd
Priansh Shah <shah@priansh.com>
2021-08-17 05:57:11 -0400

    convert TS w/ api to typescript

--- Mouseion/engine.ts
+++ Mouseion/engine.ts
@@ -1,12 +1,20 @@
 import autoBind from "auto-bind";
+import { LoginTicket } from "google-auth-library";
 import Mailbox from "./managers/mailbox";
 import { IMAPConfig } from "./post-office/types";
+import { Logger, LumberjackEmployer } from "./utils/logger";
+import sleep from "./utils/sleep";
 
+//? Mostly the core API
 export default class Engine {
+
+  readonly Log: Logger
 
   constructor(
     public mailbox: Mailbox
   ) {
+    const Lumberjack = mailbox.Registry.get("Lumberjack") as LumberjackEmployer
+    this.Log = Lumberjack("Engine")
     autoBind(this)
   }
 
@@ -18,6 +26,63 @@ export default class Engine {
     return new Engine(mailbox)
   }
 
-  //? Some API
+  //? Probably only password change and etc here
+  reconnect = this.mailbox.courier.network.connect
+  sync = {
+    immediate: this.mailbox.run,
+    start: this.mailbox.run,
+    stop: () => this.mailbox.queuedSync ? clearTimeout(this.mailbox.queuedSync) : null,
+    add: this.mailbox.sync.queueForSync,
+    remove: this.mailbox.sync.unqueueForSync
+  }
+  folders = {
+    state: async () => this.mailbox.folders.state,
+    sync: this.mailbox.folders.sync,
+    add: this.mailbox.folders.add,
+    remove: this.mailbox.folders.remove,
+    boards: this.mailbox.folders.boards,
+    all: this.mailbox.folders.all
+  }
+  resolve = {
+    messages: {
+      full: this.mailbox.resolver.messages.full,
+      content: this.mailbox.resolver.messages.content,
+      headers: this.mailbox.resolver.messages.headers,
+      envelope: this.mailbox.resolver.messages.envelope,
+    },
+    thread: {
+      full: this.mailbox.resolver.thread.full,
+      content: this.mailbox.resolver.thread.content,
+      headers: this.mailbox.resolver.thread.headers,
+    },
+    threads: {
+      latest: this.mailbox.resolver.multithread.latest
+    }
+  }
+  manage = {
+    star: this.mailbox.link.star,
+    unstar: this.mailbox.link.unstar,
+    read: this.mailbox.link.read,
+    unread: this.mailbox.link.unread,
+    archive: this.mailbox.link.archive,
+    copy: this.mailbox.link.copy,
+    move: this.mailbox.link.move,
+    delete: this.mailbox.link.delete,
+  }
+  contacts = {
+    lookup: this.mailbox.pantheon.db.contacts.search
+  }
+  trigger = {
+    register: this.mailbox.register,
+    shoot: this.mailbox.trigger
+  }
+
+  async close() {
+    await this.mailbox.close()
+    this.Log.success("Safe to exit. Killing in 1 second...")
+    await sleep(1000)
+    process.exit()
+  }
+
 
 }
