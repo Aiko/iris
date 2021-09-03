e2517e281d75613c449aa6f7539940a1e076b4ff
Priansh Shah <shah@priansh.com>
2021-09-03 09:09:51 -0400

    refactor most of app

---
+++ app-old.js
@@ -0,0 +1,202 @@
+/// //////////////////////////////////////////////////////
+/// //////////////////////////////////////////////////////
+// Set up OAuth clients
+/// //////////////////////////////////////////////////////
+/// //////////////////////////////////////////////////////
+//! gmail uses self signed certs :(
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
+Log.log("Building OAuth modules")
+const GOAuth = require('./src/oauth/goauth')
+const MSAuth = require('./src/oauth/msoauth')
+
+Log.log('Setting up GOauth')
+GOAuth(
+  '446179098641-5cafrt7dl4rsqtvi5tjccqrbknurtr7k.apps.googleusercontent.com',
+  null, // no client secret if you registered as iOS app!
+  ['https://mail.google.com']
+)
+
+Log.log('Setting up MSOauth')
+MSAuth(
+  '65b77461-4950-4abb-b571-ad129d9923a3',
+  '8154fffe-1ce5-4712-aea5-077fdcd97b9c'
+)
+/// //////////////////////////////////////////////////////
+/// //////////////////////////////////////////////////////
+
+/// //////////////////////////////////////////////////////
+/// //////////////////////////////////////////////////////
+// Require mailing tools
+/// //////////////////////////////////////////////////////
+/// //////////////////////////////////////////////////////
+Log.log('Setting up email engine IPC')
+const CarrierPigeon = require('./src/mail/email')
+Log.log('Setting up SMTP...')
+const Mailman = require('./src/mail/sendmail')
+/// //////////////////////////////////////////////////////
+/// //////////////////////////////////////////////////////
+
+/// //////////////////////////////////////////////////////
+/// //////////////////////////////////////////////////////
+// Set up cache
+/// //////////////////////////////////////////////////////
+/// //////////////////////////////////////////////////////
+Log.log('Setting up preferences IPC')
+const Prefs = require('./src/cache/prefs')('prefs.json')
+Log.log('Loading preferences...')
+Prefs.set(Prefs.load())
+Log.log('Building cache...')
+const BigBoi = require('./src/cache/big-boi-cache')('cache')
+/// //////////////////////////////////////////////////////
+/// //////////////////////////////////////////////////////
+
+/// //////////////////////////////////////////////////////
+/// //////////////////////////////////////////////////////
+// Window Controls
+/// //////////////////////////////////////////////////////
+/// //////////////////////////////////////////////////////
+// Controls the windows of our app :)
+let win
+Log.log('Setting up window controls')
+const WindowManager = require('./src/utils/window')(win)
+
+// Components and side windows of our app
+const Composer = require('./src/components/composer')
+const Calendar = require('./src/components/calendar')
+/// //////////////////////////////////////////////////////
+/// //////////////////////////////////////////////////////
+
+/// //////////////////////////////////////////////////////
+/// //////////////////////////////////////////////////////
+// Define entry scripts
+/// //////////////////////////////////////////////////////
+/// //////////////////////////////////////////////////////
+const entry = () => {
+  const signed_in = Prefs.data.authenticated
+
+  if (signed_in) {
+    Log.log('User is signed in, loading the main app.')
+    // FIXME: before deployment, remove commit_hash from url below
+    win.loadURL(`file://${__dirname}/public/index.html#${commit_hash}`, {
+      // have to pretend to be Chrome to pass OAuth
+      userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4101.0 Safari/537.36 Edg/83.0.474.0'
+    })
+  } else {
+    Log.log('User is not signed in, they will go thru the signin flow.')
+    win.loadURL('https://helloaiko.com/email/signin', {
+      userAgent: 'Aiko Mail'
+    })
+  }
+}
+ipcMain.handle('reentry', (_, __) => entry())
+/// //////////////////////////////////////////////////////
+/// //////////////////////////////////////////////////////
+
+/// //////////////////////////////////////////////////////
+/// //////////////////////////////////////////////////////
+// Define launch scripts
+/// //////////////////////////////////////////////////////
+/// //////////////////////////////////////////////////////
+//import { ElectronBlocker } from '@cliqz/adblocker-electron';
+//import fetch from 'cross-fetch'; // required 'fetch'
+const { ElectronBlocker } = require('@cliqz/adblocker-electron')
+const fetch = require('cross-fetch')
+
+const init = async () => {
+  const Sentinel = await ElectronBlocker.fromPrebuiltAdsAndTracking(fetch)
+
+  win = new BrowserWindow({
+    show: false,
+    frame: process.platform == 'darwin',
+    titleBarStyle: 'hidden',
+    backgroundColor: '#312f2e',
+    webPreferences: {
+      enableRemoteModule: true,
+      nodeIntegration: true
+      // FIXME: while this is enabled we have to be 1000% sure that
+      // we are in no way shape or form loading scripts from any
+      // external or unsafe source. However, disabling this will
+      // totally and irreversibly cripple our IPC.
+
+      // If you are coming across this on an audit please don't remove
+      // it, as instead we have opted to ensure we aren't loading any
+      // external scripts. It's a bit of a compromise we are forced to
+      // make in order to make use of TCP sockets on the backend.
+      // see: https://www.electronjs.org/docs/tutorial/security#3-enable-context-isolation-for-remote-content
+      // Without this we would have to use IPC through preload which is
+      // just as if not more so insecure :)
+
+      // Our only other alternative is to run a local web server with
+      // websockets from the backend and communicate in that manner.
+      // We have elected not to do this, but if this option is fully
+      // a pressing issue at the time of your audit, that would be our
+      // alternative course of action.
+    },
+    // TODO: icon
+    icon: process.platform == 'darwin' ? './public/assets/img/icon.png' : './public/assets/img/app-icon/square-icon-shadow.png'
+  })
+  WindowManager.setWindow(win)
+  win.maximize()
+  win.show()
+  win.focus()
+
+  entry()
+
+  Sentinel.enableBlockingInSession(win.webContents.session)
+  win.on('closed', () => {
+    win = null
+    WindowManager.setWindow(null)
+  })
+}
+/// //////////////////////////////////////////////////////
+/// //////////////////////////////////////////////////////
+
+/// //////////////////////////////////////////////////////
+/// //////////////////////////////////////////////////////
+// LAUNCH TIME 🚀🚀🚀🚀🚀🚀
+/// //////////////////////////////////////////////////////
+/// //////////////////////////////////////////////////////
+app.allowRendererProcessReuse = false
+app.on('ready', init)
+
+app.on('window-all-closed', () => {
+  // TODO: live on in the tray
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', () => {
+  if (win === null) init()
+})
+/// //////////////////////////////////////////////////////
+/// //////////////////////////////////////////////////////
+
+/// //////////////////////////////////////////////////////
+/// //////////////////////////////////////////////////////
+// Some useful variables
+/// //////////////////////////////////////////////////////
+/// //////////////////////////////////////////////////////
+module.exports = {
+  platform: process.platform
+}
+
+autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
+  // TODO: replace w/ modal
+  dialog.showMessageBox(win, {
+    type: 'question',
+    buttons: ['Update', 'Later'],
+    defaultId: 0,
+    message: `An update to Aiko Mail is available. Updates contain important security updates, vital bug fixes and new features.`,
+    title: 'Update Available'
+  }, response => {
+    if (response === 0) {
+      autoUpdater.quitAndInstall()
+    } else console.log(response)
+  })
+})
+
+if (!dev) {
+  autoUpdater.checkForUpdates()
+  setInterval(autoUpdater.checkForUpdates, 5 * 60 * 1000)
+}
+/// //////////////////////////////////////////////////////
+/// //////////////////////////////////////////////////////
--- app.js
+++ app.js
@@ -1,220 +1,166 @@
 /// //////////////////////////////////////////////////////
 /// //////////////////////////////////////////////////////
-// Utilities
-/// //////////////////////////////////////////////////////
-/// //////////////////////////////////////////////////////
-const Sentry = require('@sentry/electron')
+//? We use Sentry for security.
+//! Sentry should be the first thing to load in the entire app.
 // TODO: should also track environment
 // TODO: bug reports, managed updates, etc. for electron
+const Sentry = require('@sentry/electron')
 Sentry.init({ dsn: "https://611b04549c774cf18a3cf72636dba7cb@o342681.ingest.sentry.io/5560104" });
+
+//? Spawn a new Forest to use for the Main process's logs
+const Forest = require('./Mouseion/dist/utils/logger').default
+const forest = new Forest("logs-main-process")
+const Lumberjack = forest.Lumberjack
+const Log = Lumberjack("App")
+
+const os = require('os')
+const { app, BrowserWindow, ipcMain, dialog } = require('electron')
 /// //////////////////////////////////////////////////////
 /// //////////////////////////////////////////////////////
 
-/// //////////////////////////////////////////////////////
-/// //////////////////////////////////////////////////////
-// Logger :)
-// TODO: upgrade to Aiko Lumberjack
-const Log = require('./src/utils/logger')
-/// //////////////////////////////////////////////////////
-/// //////////////////////////////////////////////////////
+
 
 
 /// //////////////////////////////////////////////////////
 /// //////////////////////////////////////////////////////
-// FIXME: remove this code BEFORE deployment
-/// //////////////////////////////////////////////////////
-/// //////////////////////////////////////////////////////
-Log.log("Building commit hash")
-// versioning for debugging
-let commit_hash
-let dev = false
+//? Version identifier using commit
+let commit_hash, dev
 try {
   commit_hash = require('child_process')
-    .execSync('git rev-parse HEAD')
-    .toString().trim()
+                  .execSync("git rev-parse HEAD")
+                  .toString()
+                  .trim()
   dev = true
+  Log.warn("Developer mode ON - commit #", commit_hash)
 } catch (e) {
-  commit_hash = null
+  //! FIXME: remove before deployment
+  commit_hash = os.platform() + '-' + app.getVersion() + ': NOT FOR RELEASE!'
   dev = false
-}
-/// //////////////////////////////////////////////////////
-/// //////////////////////////////////////////////////////
-
-
-
-/// //////////////////////////////////////////////////////
-// Startup
-/// //////////////////////////////////////////////////////
-Log.log('Starting up')
-const os = require('os')
-const { app, BrowserWindow, ipcMain, dialog, autoUpdater } = require('electron')
-
-//? checks to make sure we're not in the midst of installation
-if (require('electron-squirrel-startup')) {
-  app.quit()
-  process.exit(0)
+  Log.log("Developer mode OFF. Performance will reflect production.")
 }
 
-Log.log("Setting up automatic updates")
-//? check for updates
-autoUpdater.on('error', Log.error)
-autoUpdater.on('checking-for-update', () => Log.log('Checking for updates...'))
-autoUpdater.on('update-available', () => Log.log('Update is available, downloading...'))
-autoUpdater.on('update-not-available', () => Log.success('App is up to date.'))
-autoUpdater.on('update-downloaded', () => Log.success('Downloaded update'))
-
-const platform = os.platform() // + '_' + os.arch()
-const version = app.getVersion()
-
-// TODO: remove before deployment
-if (!dev) commit_hash = platform + '-' + version + ': NOT FOR RELEASE!'
-
-const updateFeed = (() => {
-  return 'https://aiko-mail-update-service.herokuapp.com/update/'+platform+'/'+version;
-})()
-Log.log("Fetches updates from", updateFeed)
-autoUpdater.setFeedURL({
-  url: updateFeed
-})
-
+//? App Manager tool that handles updates
+const AppManager = require('./src/utils/app-management')(Lumberjack, dev ? "Dev" : "Stable")
 /// //////////////////////////////////////////////////////
 /// //////////////////////////////////////////////////////
 
+
+
 /// //////////////////////////////////////////////////////
 /// //////////////////////////////////////////////////////
-// Set up OAuth clients
-/// //////////////////////////////////////////////////////
-/// //////////////////////////////////////////////////////
-//! gmail uses self signed certs :(
+//? OAuth modules handle servicing OAuth requests
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
-Log.log("Building OAuth modules")
+Log.log("Building OAuth modules.")
 const GOAuth = require('./src/oauth/goauth')
-const MSAuth = require('./src/oauth/msoauth')
+const MSOAuth = require('./src/oauth/msoauth')
 
-Log.log('Setting up GOauth')
+Log.log("Initializing OAuth modules.")
 GOAuth(
   '446179098641-5cafrt7dl4rsqtvi5tjccqrbknurtr7k.apps.googleusercontent.com',
-  null, // no client secret if you registered as iOS app!
+  null, //! no client secret: register it as an iOS app
   ['https://mail.google.com']
 )
-
-Log.log('Setting up MSOauth')
-MSAuth(
+MSOAuth(
   '65b77461-4950-4abb-b571-ad129d9923a3',
   '8154fffe-1ce5-4712-aea5-077fdcd97b9c'
 )
 /// //////////////////////////////////////////////////////
 /// //////////////////////////////////////////////////////
 
+
+
+
 /// //////////////////////////////////////////////////////
 /// //////////////////////////////////////////////////////
-// Require mailing tools
-/// //////////////////////////////////////////////////////
-/// //////////////////////////////////////////////////////
-Log.log('Setting up email engine IPC')
+//? Email modules that enable IMAP/SMTP
+Log.log("Building IMAP/SMTP modules.")
 const CarrierPigeon = require('./src/mail/email')
-Log.log('Setting up SMTP...')
 const Mailman = require('./src/mail/sendmail')
 /// //////////////////////////////////////////////////////
 /// //////////////////////////////////////////////////////
 
-/// //////////////////////////////////////////////////////
-/// //////////////////////////////////////////////////////
-// Set up cache
-/// //////////////////////////////////////////////////////
-/// //////////////////////////////////////////////////////
-Log.log('Setting up preferences IPC')
-const Prefs = require('./src/cache/prefs')('prefs.json')
-Log.log('Loading preferences...')
-Prefs.set(Prefs.load())
-Log.log('Building cache...')
-const BigBoi = require('./src/cache/big-boi-cache')('cache')
-/// //////////////////////////////////////////////////////
-/// //////////////////////////////////////////////////////
+
+
 
 /// //////////////////////////////////////////////////////
 /// //////////////////////////////////////////////////////
-// Window Controls
+//? Caches, preferences, storage
+Log.log("Building cache modules.")
+const Prefs = require('./src/cache/prefs')('prefs.json')
+const BigBoi = require('./src/cache/big-boi-cache')('cache')
+//? Load preferences
+Prefs.set(Prefs.load())
 /// //////////////////////////////////////////////////////
 /// //////////////////////////////////////////////////////
-// Controls the windows of our app :)
-let win
-Log.log('Setting up window controls')
+
+
+
+
+/// //////////////////////////////////////////////////////
+/// //////////////////////////////////////////////////////
+//? Window controls for the main window
+let win //! our main window tool
 const WindowManager = require('./src/utils/window')(win)
 
-// Components and side windows of our app
-const Composer = require('./src/components/composer')
+//? Components that spawn side windows receive their own window controls
+const ComposerManager = require('./src/components/composer')
 const Calendar = require('./src/components/calendar')
 /// //////////////////////////////////////////////////////
 /// //////////////////////////////////////////////////////
 
+
+
+
 /// //////////////////////////////////////////////////////
 /// //////////////////////////////////////////////////////
-// Define entry scripts
-/// //////////////////////////////////////////////////////
-/// //////////////////////////////////////////////////////
-const entry = () => {
+//? Entry script for the main window
+const GLOBAL_DISABLE_AUTH=true
+const entry = (disable_auth=GLOBAL_DISABLE_AUTH) => {
   const signed_in = Prefs.data.authenticated
 
-  if (signed_in) {
-    Log.log('User is signed in, loading the main app.')
-    // FIXME: before deployment, remove commit_hash from url below
+  if (signed_in || disable_auth) {
+    Log.success("User is signed in, loading their inbox.")
+    //! FIXME: before deployment, remove commit_hash from url below
     win.loadURL(`file://${__dirname}/public/index.html#${commit_hash}`, {
-      // have to pretend to be Chrome to pass OAuth
+      //? have to pretend to be Chrome to pass OAuth
       userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4101.0 Safari/537.36 Edg/83.0.474.0'
     })
   } else {
-    Log.log('User is not signed in, they will go thru the signin flow.')
-    win.loadURL('https://helloaiko.com/email/signin', {
+    Log.warn("User is not signed in, loading the signin flow.")
+    win.loadURL("https://helloaiko.com/email/signin", {
       userAgent: 'Aiko Mail'
     })
   }
 }
-ipcMain.handle('reentry', (_, __) => entry())
+
+ipcMain.handle('reentry', () => entry())
 /// //////////////////////////////////////////////////////
 /// //////////////////////////////////////////////////////
 
+
+
+
 /// //////////////////////////////////////////////////////
 /// //////////////////////////////////////////////////////
-// Define launch scripts
-/// //////////////////////////////////////////////////////
-/// //////////////////////////////////////////////////////
-//import { ElectronBlocker } from '@cliqz/adblocker-electron';
-//import fetch from 'cross-fetch'; // required 'fetch'
+//? Initialization script for launching the main window
+
+//? Adblock to block email trackers
 const { ElectronBlocker } = require('@cliqz/adblocker-electron')
+//? Fetch tooling for requests
 const fetch = require('cross-fetch')
 
 const init = async () => {
-  const Sentinel = await ElectronBlocker.fromPrebuiltAdsAndTracking(fetch)
+  const SentinelAdblock = await ElectronBlocker.fromPrebuiltAdsAndTracking(fetch)
 
   win = new BrowserWindow({
-    show: false,
-    frame: process.platform == 'darwin',
-    titleBarStyle: 'hidden',
-    backgroundColor: '#312f2e',
+    show: false, //? don't show it until after we load window controls
+    frame: process.platform == 'darwin', //? frameless on windows
+    titleBarStyle: "hidden",
+    backgroundColor: "#312f2e", //? background for the app
     webPreferences: {
-      enableRemoteModule: true,
-      nodeIntegration: true
-      // FIXME: while this is enabled we have to be 1000% sure that
-      // we are in no way shape or form loading scripts from any
-      // external or unsafe source. However, disabling this will
-      // totally and irreversibly cripple our IPC.
-
-      // If you are coming across this on an audit please don't remove
-      // it, as instead we have opted to ensure we aren't loading any
-      // external scripts. It's a bit of a compromise we are forced to
-      // make in order to make use of TCP sockets on the backend.
-      // see: https://www.electronjs.org/docs/tutorial/security#3-enable-context-isolation-for-remote-content
-      // Without this we would have to use IPC through preload which is
-      // just as if not more so insecure :)
-
-      // Our only other alternative is to run a local web server with
-      // websockets from the backend and communicate in that manner.
-      // We have elected not to do this, but if this option is fully
-      // a pressing issue at the time of your audit, that would be our
-      // alternative course of action.
+      enableRemoteModule: true, //! FIXME: you know this is bad...
+      nodeIntegration: true, //! FIXME: migrate fully to websockets
     },
-    // TODO: icon
     icon: process.platform == 'darwin' ? './public/assets/img/icon.png' : './public/assets/img/app-icon/square-icon-shadow.png'
   })
   WindowManager.setWindow(win)
@@ -224,8 +170,8 @@ const init = async () => {
 
   entry()
 
-  Sentinel.enableBlockingInSession(win.webContents.session)
-  win.on('closed', () => {
+  SentinelAdblock.enableBlockingInSession(win.webContents.session)
+  win.on("closed", () => {
     win = null
     WindowManager.setWindow(null)
   })
@@ -233,52 +179,35 @@ const init = async () => {
 /// //////////////////////////////////////////////////////
 /// //////////////////////////////////////////////////////
 
-/// //////////////////////////////////////////////////////
-/// //////////////////////////////////////////////////////
-// LAUNCH TIME 🚀🚀🚀🚀🚀🚀
+
+//! TODO: refactor below this line
+
+
 /// //////////////////////////////////////////////////////
 /// //////////////////////////////////////////////////////
 app.allowRendererProcessReuse = false
-app.on('ready', init)
+app.on("ready", init)
 
-app.on('window-all-closed', () => {
+app.on("window-all-closed", () => {
   // TODO: live on in the tray
   if (process.platform !== 'darwin') app.quit()
 })
 
-app.on('activate', () => {
+app.on("activate", () => {
   if (win === null) init()
 })
 /// //////////////////////////////////////////////////////
 /// //////////////////////////////////////////////////////
 
-/// //////////////////////////////////////////////////////
-/// //////////////////////////////////////////////////////
-// Some useful variables
+
+
+
 /// //////////////////////////////////////////////////////
 /// //////////////////////////////////////////////////////
 module.exports = {
   platform: process.platform
 }
 
-autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
-  // TODO: replace w/ modal
-  dialog.showMessageBox(win, {
-    type: 'question',
-    buttons: ['Update', 'Later'],
-    defaultId: 0,
-    message: `An update to Aiko Mail is available. Updates contain important security updates, vital bug fixes and new features.`,
-    title: 'Update Available'
-  }, response => {
-    if (response === 0) {
-      autoUpdater.quitAndInstall()
-    } else console.log(response)
-  })
-})
-
-if (!dev) {
-  autoUpdater.checkForUpdates()
-  setInterval(autoUpdater.checkForUpdates, 5 * 60 * 1000)
-}
+if (!dev) AppManager.checkForUpdates()
 /// //////////////////////////////////////////////////////
 /// //////////////////////////////////////////////////////
