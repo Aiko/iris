938f605a8bfecf88ef54ff5ec68340a8c89bbdbf
Priansh Shah <shah@priansh.com>
2021-09-07 07:50:13 -0400

    translate calendar to ts

---
+++ src/components/calendar.ts
@@ -0,0 +1,42 @@
+import SecureCommunications from '../utils/comms'
+import WindowManager from '../utils/window-manager'
+import Register from '../../Mouseion/managers/register'
+import autoBind from 'auto-bind'
+
+export default class Calendar {
+  private readonly comms: SecureCommunications
+  private windowManager: WindowManager | null
+
+  constructor(
+    private readonly Registry: Register,
+  ) {
+    this.comms = Registry.get("Inbox Communications") as SecureCommunications
+
+    this.comms.register("please open the calendar", this.open)
+
+    autoBind(this)
+  }
+
+  private open({bang}: {bang: string}) {
+    const win = WindowManager.newWindow({
+      height: 600, width: 800
+    })
+
+    this.windowManager = new WindowManager(this.Registry, win, 'calendar-' + bang)
+    this.windowManager.window = win
+
+    win.loadURL(`file://${__dirname}/../../public/calendar.html#${bang}`, {
+      userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4101.0 Safari/537.36 Edg/83.0.474.0'
+    })
+
+    win.show()
+    win.focus()
+
+    win.on("closed", () => {
+      this.windowManager = null
+    })
+
+    return {bang,}
+  }
+
+}
