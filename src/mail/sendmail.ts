d33d686c96430a0a65c96e634b96edfb6627ed17
Priansh Shah <shah@priansh.com>
2021-09-07 01:34:06 -0400

    convert sendmail to ts

---
+++ src/mail/sendmail.ts
@@ -0,0 +1,86 @@
+import nodemailer from "nodemailer"
+import inlineBase64 from 'nodemailer-plugin-inline-base64'
+import inlineCss from 'nodemailer-juice'
+import SecureCommunications from '../utils/comms'
+import Register from '../../Mouseion/dist/managers/register'
+import { IMAPConfig } from '../../Mouseion/post-office/types'
+import Mail from "nodemailer/lib/mailer"
+import autoBind from "auto-bind"
+
+export default class CarrierPigeon {
+  private comms: SecureCommunications
+
+  constructor(
+    Registry: Register
+  ) {
+    this.comms = Registry.get("Inbox Communications") as SecureCommunications
+
+    this.comms.register("please send an email", this.send)
+    this.comms.register("please test SMTP connection", this.test)
+
+    autoBind(this)
+  }
+
+  private getTransporter(config: Partial<IMAPConfig>): Mail {
+    let transporter: Mail;
+    switch(config.provider) {
+
+      case "google": transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          type: "OAuth2",
+          user: config.user,
+          accessToken: config.oauth
+        }
+      }); break;
+
+      case "outlook": transporter = nodemailer.createTransport({
+        service: 'hotmail',
+        auth: {
+          type: 'OAuth2',
+          user: config.user,
+          accessToken: config.oauth
+        }
+      }); break;
+
+      case "microsoft": transporter = nodemailer.createTransport({
+        service: "hotmail",
+        auth: {
+          user: config.user,
+          pass: config.pass
+        }
+      })
+
+      default: transporter = nodemailer.createTransport({
+        host: config.host,
+        port: config.port,
+        secure: config.secure,
+        auth: {
+          user: config.user,
+          pass: config.pass
+        }
+      })
+
+    }
+
+    transporter.use("compile", inlineBase64())
+    transporter.use("compile", inlineCss({ inlinePseudoElements: true }))
+
+    return transporter
+  }
+
+  private async send({mail, config}: {mail: any, config: Partial<IMAPConfig>}) {
+    const transporter = this.getTransporter(config)
+    return await new Promise((s, _) => {
+      transporter.sendMail(mail, (error, info) => error ? s({ error,  }) : s(info))
+    })
+  }
+
+  private async test(config: Partial<IMAPConfig>) {
+    const transporter = this.getTransporter(config)
+    return await new Promise((s, _) => {
+      transporter.verify((error, success) => error ? s({ error,  }) : s({ valid: success }))
+    })
+  }
+
+}
