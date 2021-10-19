4b207b8a3d6e2cf415ae3f48910b39e010cbb0fb
Priansh Shah <shah@priansh.com>
2021-10-19 01:50:58 -0400

    icon definitions for LTE support

---
+++ public/assets/js/common/icon-definitions.js
@@ -0,0 +1,503 @@
+const Icons = {
+  ".DS_Store": {
+    base: ".DS_Store",
+    light: ".DS_Store",
+    dark: ".DS_Store"
+  },
+  "admin.svg": {
+    base: "admin.svg",
+    light: "admin-lt.svg",
+    dark: "admin.svg"
+  },
+  "align-center.svg": {
+    base: "align-center.svg",
+    light: "align-center-lt.svg",
+    dark: "align-center.svg"
+  },
+  "align-left.svg": {
+    base: "align-left.svg",
+    light: "align-left-lt.svg",
+    dark: "align-left.svg"
+  },
+  "align-right.svg": {
+    base: "align-right.svg",
+    light: "align-right-lt.svg",
+    dark: "align-right.svg"
+  },
+  "arrow-down.svg": {
+    base: "arrow-down.svg",
+    light: "arrow-down-lt.svg",
+    dark: "arrow-down.svg"
+  },
+  "arrow-up.svg": {
+    base: "arrow-up.svg",
+    light: "arrow-up-lt.svg",
+    dark: "arrow-up.svg"
+  },
+  "attach.svg": {
+    base: "attach.svg",
+    light: "attach.svg",
+    dark: "attach.svg"
+  },
+  "badges-attach.svg": {
+    base: "badges-attach.svg",
+    light: "badges-attach-lt.svg",
+    dark: "badges-attach.svg"
+  },
+  "badges-bcc.svg": {
+    base: "badges-bcc.svg",
+    light: "badges-bcc-lt.svg",
+    dark: "badges-bcc.svg"
+  },
+  "badges-event-blue.svg": {
+    base: "badges-event-blue.svg",
+    light: "badges-event-blue-lt.svg",
+    dark: "badges-event-blue.svg"
+  },
+  "badges-event.svg": {
+    base: "badges-event.svg",
+    light: "badges-event-lt.svg",
+    dark: "badges-event.svg"
+  },
+  "badges-star.svg": {
+    base: "badges-star.svg",
+    light: "badges-star-lt.svg",
+    dark: "badges-star.svg"
+  },
+  "badges-tracker.svg": {
+    base: "badges-tracker.svg",
+    light: "badges-tracker-lt.svg",
+    dark: "badges-tracker.svg"
+  },
+  "blue-check.svg": {
+    base: "blue-check.svg",
+    light: "blue-check-lt.svg",
+    dark: "blue-check.svg"
+  },
+  "board-archive.svg": {
+    base: "board-archive.svg",
+    light: "board-archive-lt.svg",
+    dark: "board-archive.svg"
+  },
+  "board-dots.svg": {
+    base: "board-dots.svg",
+    light: "board-dots-lt.svg",
+    dark: "board-dots.svg"
+  },
+  "board-dots2.svg": {
+    base: "board-dots2.svg",
+    light: "board-dots2-lt.svg",
+    dark: "board-dots2.svg"
+  },
+  "boards-add.svg": {
+    base: "boards-add.svg",
+    light: "boards-add-lt.svg",
+    dark: "boards-add.svg"
+  },
+  "bold.svg": {
+    base: "bold.svg",
+    light: "bold-lt.svg",
+    dark: "bold.svg"
+  },
+  "cal.svg": {
+    base: "cal.svg",
+    light: "cal-lt.svg",
+    dark: "cal.svg"
+  },
+  "calendar.svg": {
+    base: "calendar.svg",
+    light: "calendar-lt.svg",
+    dark: "calendar.svg"
+  },
+  "check-circle.svg": {
+    base: "check-circle.svg",
+    light: "check-circle-lt.svg",
+    dark: "check-circle.svg"
+  },
+  "check-square.svg": {
+    base: "check-square.svg",
+    light: "check-square-lt.svg",
+    dark: "check-square.svg"
+  },
+  "clipboard.svg": {
+    base: "clipboard.svg",
+    light: "clipboard.svg",
+    dark: "clipboard.svg"
+  },
+  "close-g.png": {
+    base: "close-g.png",
+    light: "close-g-lt.png",
+    dark: "close-g.png"
+  },
+  "code.svg": {
+    base: "code.svg",
+    light: "code-lt.svg",
+    dark: "code.svg"
+  },
+  "compose-attach.svg": {
+    base: "compose-attach.svg",
+    light: "compose-attach-lt.svg",
+    dark: "compose-attach.svg"
+  },
+  "compose-send.svg": {
+    base: "compose-send.svg",
+    light: "compose-send-lt.svg",
+    dark: "compose-send.svg"
+  },
+  "email-schedule.svg": {
+    base: "email-schedule.svg",
+    light: "email-schedule-lt.svg",
+    dark: "email-schedule.svg"
+  },
+  "eplus.svg": {
+    base: "eplus.svg",
+    light: "eplus-lt.svg",
+    dark: "eplus.svg"
+  },
+  "event-white.svg": {
+    base: "event-white.svg",
+    light: "event-white-lt.svg",
+    dark: "event-white.svg"
+  },
+  "filter-inbox.svg": {
+    base: "filter-inbox.svg",
+    light: "filter-inbox-lt.svg",
+    dark: "filter-inbox.svg"
+  },
+  "grey-x.svg": {
+    base: "grey-x.svg",
+    light: "grey-x-lt.svg",
+    dark: "grey-x.svg"
+  },
+  "header-bell.svg": {
+    base: "header-bell.svg",
+    light: "header-bell-lt.svg",
+    dark: "header-bell.svg"
+  },
+  "header-calendar.svg": {
+    base: "header-calendar.svg",
+    light: "header-calendar-lt.svg",
+    dark: "header-calendar.svg"
+  },
+  "header-plus.svg": {
+    base: "header-plus.svg",
+    light: "header-plus-lt.svg",
+    dark: "header-plus.svg"
+  },
+  "header-search.svg": {
+    base: "header-search.svg",
+    light: "header-search-lt.svg",
+    dark: "header-search.svg"
+  },
+  "header-settings.svg": {
+    base: "header-settings.svg",
+    light: "header-settings-lt.svg",
+    dark: "header-settings.svg"
+  },
+  "header-user.svg": {
+    base: "header-user.svg",
+    light: "header-user-lt.svg",
+    dark: "header-user.svg"
+  },
+  "image.svg": {
+    base: "image.svg",
+    light: "image-lt.svg",
+    dark: "image.svg"
+  },
+  "info.svg": {
+    base: "info.svg",
+    light: "info-lt.svg",
+    dark: "info.svg"
+  },
+  "integrations.svg": {
+    base: "integrations.svg",
+    light: "integrations-lt.svg",
+    dark: "integrations.svg"
+  },
+  "italic.svg": {
+    base: "italic.svg",
+    light: "italic-lt.svg",
+    dark: "italic.svg"
+  },
+  "link-3.svg": {
+    base: "link-3.svg",
+    light: "link-3-lt.svg",
+    dark: "link-3.svg"
+  },
+  "link.svg": {
+    base: "link.svg",
+    light: "link-lt.svg",
+    dark: "link.svg"
+  },
+  "list-num.svg": {
+    base: "list-num.svg",
+    light: "list-num-lt.svg",
+    dark: "list-num.svg"
+  },
+  "list.svg": {
+    base: "list.svg",
+    light: "list-lt.svg",
+    dark: "list.svg"
+  },
+  "music.svg": {
+    base: "music.svg",
+    light: "music-lt.svg",
+    dark: "music.svg"
+  },
+  "new-board.svg": {
+    base: "new-board.svg",
+    light: "new-board-lt.svg",
+    dark: "new-board.svg"
+  },
+  "notif-click.svg": {
+    base: "notif-click.svg",
+    light: "notif-click-lt.svg",
+    dark: "notif-click.svg"
+  },
+  "notif-eye.svg": {
+    base: "notif-eye.svg",
+    light: "notif-eye-lt.svg",
+    dark: "notif-eye.svg"
+  },
+  "notif-invite.svg": {
+    base: "notif-invite.svg",
+    light: "notif-invite-lt.svg",
+    dark: "notif-invite.svg"
+  },
+  "options-forward.png": {
+    base: "options-forward.png",
+    light: "options-forward-lt.png",
+    dark: "options-forward.png"
+  },
+  "options-replied.png": {
+    base: "options-replied.png",
+    light: "options-replied.png",
+    dark: "options-replied.png"
+  },
+  "options-reply.png": {
+    base: "options-reply.png",
+    light: "options-reply-lt.png",
+    dark: "options-reply.png"
+  },
+  "options-replya.png": {
+    base: "options-replya.png",
+    light: "options-replya-lt.png",
+    dark: "options-replya.png"
+  },
+  "options-star.svg": {
+    base: "options-star.svg",
+    light: "options-star-lt.svg",
+    dark: "options-star.svg"
+  },
+  "options-trash.svg": {
+    base: "options-trash.svg",
+    light: "options-trash-lt.svg",
+    dark: "options-trash.svg"
+  },
+  "options-unsub.svg": {
+    base: "options-unsub.svg",
+    light: "options-unsub-lt.svg",
+    dark: "options-unsub.svg"
+  },
+  "out.svg": {
+    base: "out.svg",
+    light: "out-lt.svg",
+    dark: "out.svg"
+  },
+  "q.svg": {
+    base: "q.svg",
+    light: "q-lt.svg",
+    dark: "q.svg"
+  },
+  "refresh.svg": {
+    base: "refresh.svg",
+    light: "refresh-lt.svg",
+    dark: "refresh.svg"
+  },
+  "s-invite.svg": {
+    base: "s-invite.svg",
+    light: "s-invite-lt.svg",
+    dark: "s-invite.svg"
+  },
+  "settings-billing.svg": {
+    base: "settings-billing.svg",
+    light: "settings-billing-lt.svg",
+    dark: "settings-billing.svg"
+  },
+  "settings-customize.svg": {
+    base: "settings-customize.svg",
+    light: "settings-customize-lt.svg",
+    dark: "settings-customize.svg"
+  },
+  "settings-email.svg": {
+    base: "settings-email.svg",
+    light: "settings-email-lt.svg",
+    dark: "settings-email.svg"
+  },
+  "settings-team.svg": {
+    base: "settings-team.svg",
+    light: "settings-team-lt.svg",
+    dark: "settings-team.svg"
+  },
+  "settings-templates.svg": {
+    base: "settings-templates.svg",
+    light: "settings-templates-lt.svg",
+    dark: "settings-templates.svg"
+  },
+  "sidebar-archive.svg": {
+    base: "sidebar-archive.svg",
+    light: "sidebar-archive-lt.svg",
+    dark: "sidebar-archive.svg"
+  },
+  "sidebar-collapse.svg": {
+    base: "sidebar-collapse.svg",
+    light: "sidebar-collapse-lt.svg",
+    dark: "sidebar-collapse.svg"
+  },
+  "sidebar-drafts.svg": {
+    base: "sidebar-drafts.svg",
+    light: "sidebar-drafts-lt.svg",
+    dark: "sidebar-drafts.svg"
+  },
+  "sidebar-edit.svg": {
+    base: "sidebar-edit.svg",
+    light: "sidebar-edit-lt.svg",
+    dark: "sidebar-edit.svg"
+  },
+  "sidebar-help.svg": {
+    base: "sidebar-help.svg",
+    light: "sidebar-help-lt.svg",
+    dark: "sidebar-help.svg"
+  },
+  "sidebar-home.svg": {
+    base: "sidebar-home.svg",
+    light: "sidebar-home-lt.svg",
+    dark: "sidebar-home.svg"
+  },
+  "sidebar-invite.svg": {
+    base: "sidebar-invite.svg",
+    light: "sidebar-invite-lt.svg",
+    dark: "sidebar-invite.svg"
+  },
+  "sidebar-send.svg": {
+    base: "sidebar-send.svg",
+    light: "sidebar-send-lt.svg",
+    dark: "sidebar-send.svg"
+  },
+  "sidebar-settings.svg": {
+    base: "sidebar-settings.svg",
+    light: "sidebar-settings-lt.svg",
+    dark: "sidebar-settings.svg"
+  },
+  "sidebar-spam.svg": {
+    base: "sidebar-spam.svg",
+    light: "sidebar-spam-lt.svg",
+    dark: "sidebar-spam.svg"
+  },
+  "sidebar-star.svg": {
+    base: "sidebar-star.svg",
+    light: "sidebar-star-lt.svg",
+    dark: "sidebar-star.svg"
+  },
+  "sidebar-support.svg": {
+    base: "sidebar-support.svg",
+    light: "sidebar-support-lt.svg",
+    dark: "sidebar-support.svg"
+  },
+  "sidebar-trash.svg": {
+    base: "sidebar-trash.svg",
+    light: "sidebar-trash-lt.svg",
+    dark: "sidebar-trash.svg"
+  },
+  "sign.svg": {
+    base: "sign.svg",
+    light: "sign-lt.svg",
+    dark: "sign.svg"
+  },
+  "smile.svg": {
+    base: "smile.svg",
+    light: "smile-lt.svg",
+    dark: "smile.svg"
+  },
+  "sort.svg": {
+    base: "sort.svg",
+    light: "sort-lt.svg",
+    dark: "sort.svg"
+  },
+  "t.svg": {
+    base: "t.svg",
+    light: "t-lt.svg",
+    dark: "t.svg"
+  },
+  "th.svg": {
+    base: "th.svg",
+    light: "th-lt.svg",
+    dark: "th.svg"
+  },
+  "thread.svg": {
+    base: "thread.svg",
+    light: "thread-lt.svg",
+    dark: "thread.svg"
+  },
+  "toggle-left.svg": {
+    base: "toggle-left.svg",
+    light: "toggle-left-lt.svg",
+    dark: "toggle-left.svg"
+  },
+  "toggle-right.svg": {
+    base: "toggle-right.svg",
+    light: "toggle-right-lt.svg",
+    dark: "toggle-right.svg"
+  },
+  "top-close.png": {
+    base: "top-close.png",
+    light: "top-close-lt.png",
+    dark: "top-close.png"
+  },
+  "top-expand.png": {
+    base: "top-expand.png",
+    light: "top-expand-lt.png",
+    dark: "top-expand.png"
+  },
+  "top-minimize.png": {
+    base: "top-minimize.png",
+    light: "top-minimize-lt.png",
+    dark: "top-minimize.png"
+  },
+  "track.svg": {
+    base: "track.svg",
+    light: "track-lt.svg",
+    dark: "track.svg"
+  },
+  "underline.svg": {
+    base: "underline.svg",
+    light: "underline-lt.svg",
+    dark: "underline.svg"
+  },
+  "up.svg": {
+    base: "up.svg",
+    light: "up-lt.svg",
+    dark: "up.svg"
+  },
+  "user-chat.svg": {
+    base: "user-chat.svg",
+    light: "user-chat-lt.svg",
+    dark: "user-chat.svg"
+  },
+  "user-mail.svg": {
+    base: "user-mail.svg",
+    light: "user-mail-lt.svg",
+    dark: "user-mail.svg"
+  },
+  "x-white.svg": {
+    base: "x-white.svg",
+    light: "x-white-lt.svg",
+    dark: "x-white.svg"
+  },
+  "zap.svg": {
+    base: "zap.svg",
+    light: "zap-lt.svg",
+    dark: "zap.svg"
+  },
+}
+window.Icons = Icons;
