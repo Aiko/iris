7de534d9ab55957a58ad698d01b017b9045e441c
cuuupid <shah@priansh.com>
2022-01-10 20:31:26 -0500

    fix updates and hash

--- app.ts
+++ app.ts
@@ -75,7 +75,7 @@ try {
   Log.warn("Developer mode ON - commit #", commit_hash)
 } catch (e) {
   //! FIXME: remove before deployment
-  commit_hash = os.platform() + '-' + app.getVersion() + ':INTERNAL🦭'
+  commit_hash = os.platform() + '-' + app.getVersion() + ':INTERNAL'
   dev = false
   Log.log("Developer mode OFF. Performance will reflect production.")
 }
