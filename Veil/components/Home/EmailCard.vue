d402fd5cb7518b73788faa6cf5e8ed6c915d3a61
Ruben <rubencharlestouitou@gmail.com>
2022-10-02 19:30:03 -0500

    Working on email card

--- Veil/components/Home/EmailCard.vue
+++ Veil/components/Home/EmailCard.vue
@@ -4,6 +4,29 @@
 
 <template>
   <div class="email-card">
+    <div class="row">
+      <div class="col-8 p0 sender">
+        Ruben Touitou from Aiko Mail
+      </div>
+      <div class="col-4 p0 date">
+        Yesterday 9:36PM
+      </div>
+    </div>
+    <div class="subject">
+      Meeting request from rubentouitou.com
+    </div>
+    <div class="preview">
+      Hi this is a reminder that this is a preview, not the full email, but when you click on quick reply, you can
+      actually see all of it and scroll through its very nice
+    </div>
+    <div class="bottom">
+      <div class="quick-action">
+        Quick Reply
+      </div>
+      <div class="actions">
+        reply
+      </div>
+    </div>
 
   </div>
 </template>
@@ -13,12 +36,76 @@
   position: relative;
   background: #fff;
   border-radius: 5px;
-  cursor: default;
+  user-select: none;
   margin-bottom: 15px;
-  background: var(--main-bg-color);
+  width: 100%;
+  background: var(--secondary-background-color);
   padding: 8px 13px 0 10px;
   overflow: hidden;
+  font-size: var(--body-font-size);
   contain: layout;
+}
+
+.email-card .top {
+  display: inline-flex;
+}
+
+.email-card .sender {
+  color: var(--primary-font-color);
+  font-size: 14px;
+  white-space: nowrap;
+  line-height: 20px;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  text-align: left;
+  margin-bottom: 0;
+  letter-spacing: .4px;
+  font-weight: 500;
+}
+
+.email-card .date {
+  color: var(--primary-font-color);
+  font-size: 13px;
+  text-align: right;
+  font-weight: 500;
+  white-space: nowrap;
+}
+
+.email-card .preview {
+  color: var(--primary-font-color);
+  font-size: var(--small-font-size);
+  filter: brightness(0.8);
+  height: 33px;
+  overflow: hidden;
+  line-height: 16px;
+  margin-bottom: 3px;
+  text-overflow: ellipsis;
+  letter-spacing: .1px;
+  width: 100%;
   transition: .2s;
+}
+
+.email-card .subject {
+  font-size: 14px;
+  margin-bottom: 2px;
+  font-weight: 600;
+  height: 20px;
+  overflow: hidden;
+  width: 100%;
+  text-overflow: ellipsis;
+  margin-top: -1px;
+  white-space: nowrap;
+  letter-spacing: .4px;
+  color: var(--strong-font-color);
+}
+
+.email-card .bottom {
+  box-shadow: inset 5px 22px 40px -20px #00000073 !important;
+  user-select: none;
+  width: 283px;
+  height: 27px;
+  margin-left: -25px;
+  border-radius: 0 0 5px 5px;
+  margin-top: 2px;
 }
 </style>
--- Veil/components/Sidebar/Sidebar.vue
+++ Veil/components/Sidebar/Sidebar.vue
@@ -16,6 +16,9 @@ import NavLinkHome from '@Veil/components/Sidebar/NavLinkHome.vue';
       <NavLink text="Trash" />
     </div>
     <div class="bottom">
+      <div class="sidebar-collapse" title="Collapse sidebar">
+        <img src="@Veil/assets/icons/Sidebar/sidebar-collapse.svg" class="icon" />
+      </div>
       <a><img src="@Veil/assets/icons/Sidebar/calendar.svg" class="icon" /></a>
       <a><img src="@Veil/assets/icons/Sidebar/settings.svg" class="icon" /></a>
       <a><img src="@Veil/assets/icons/Sidebar/documentation.svg" class="icon docu" /></a>
@@ -45,6 +48,32 @@ import NavLinkHome from '@Veil/components/Sidebar/NavLinkHome.vue';
   user-select: none;
 }
 
+.sidebar-collapse {
+  position: absolute;
+  right: 0;
+  top: 0;
+  margin-top: -10px;
+  margin-right: -7px;
+  z-index: 1;
+  padding: 0 2px;
+  height: 19px;
+  cursor: default;
+  background: var(--secondary-background-color);
+  border-radius: 5px;
+}
+
+.sidebar-collapse img {
+  width: 15px !important;
+  margin-top: -8px;
+  opacity: .3;
+  transition: .2s;
+}
+
+.sidebar-collapse img:hover {
+  opacity: 1;
+  transition: .2s;
+}
+
 .compose {
   margin-top: 0;
 }
@@ -56,6 +85,8 @@ import NavLinkHome from '@Veil/components/Sidebar/NavLinkHome.vue';
 .bottom {
   margin-bottom: 20px;
   position: absolute;
+  border-top: 1px solid var(--secondary-background-color);
+  padding-top: 20px;
   bottom: 0;
   left: 0;
   background: var(--primary-background-color);
