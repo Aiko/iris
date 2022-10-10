4c5e6ec3f04405cfffa35aeeaaf97660b5a1d80b
Ruben <rubencharlestouitou@gmail.com>
2022-10-10 17:03:13 -0500

    [Design] Settings Part 1

--- Veil/views/Settings.vue
+++ Veil/views/Settings.vue
@@ -1,18 +1,25 @@
 <script lang="ts" setup>
 import { isRegularView, selectedModal, Modal } from '@Veil/state/sections'
+import NavLink from '../components/Sidebar/NavLink.vue'
 </script>
 
 <template>
   <div class="settings" v-if="true">
-    <div class="boards">
+    <div class="sidebar">
+      <h1>Settings</h1>
+      <NavLink>Account</NavLink>
+      <NavLink>Appearance</NavLink>
+      <NavLink>Team</NavLink>
+      <NavLink>Spaces</NavLink>
+      <NavLink>Unsubscribe</NavLink>
+      <NavLink>Templates</NavLink>
+      <NavLink>Billing</NavLink>
+      <NavLink>Integrations</NavLink>
+      <NavLink>Others</NavLink>
 
-      <!--Boards are normal unless they have 'inbox' attribute-->
-      <Board is-inbox />
 
-      <!-- 'isRegular' is for regular, non-kanban view-->
-      <Board v-if="!isRegularView" />
-      <AddBoard v-if="!isRegularView" @click="selectedModal = Modal.AddBoard" />
-      <SideEmail v-if="isRegularView" />
+    </div>
+    <div class="content">
 
     </div>
   </div>
@@ -24,5 +31,32 @@ import { isRegularView, selectedModal, Modal } from '@Veil/state/sections'
   overflow-y: scroll;
   height: 100%;
   display: inline-flex;
+  background-color: var(--secondary-background-color);
+  border: 3px solid var(--primary-background-color);
+}
+
+.sidebar {
+  width: 130px;
+  height: 100%;
+  background-color: var(--primary-background-color);
+  padding: 8px;
+  position: relative;
+  box-shadow: var(--sidebar-shadow);
+  z-index: 1;
+  -webkit-touch-callout: none;
+  -webkit-user-select: none;
+  -khtml-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+
+.sidebar a {
+  display: block;
+}
+
+.sidebar h1 {
+  font-size: 18px;
+  margin-bottom: 30px;
 }
 </style>
