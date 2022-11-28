b04c1c85f929130da183057d3973cf9b2f39d64b
Ruben Touitou <rubencharlestouitou@gmail.com>
2022-11-28 10:28:51 +0200

    tost fix

--- Veil/components/Header/Toast.vue
+++ Veil/components/Header/Toast.vue
@@ -9,39 +9,45 @@ import ButtonPrimary from "@Veil/components/Base/ButtonPrimary.vue";
 
   <!-- TOAST Sending Email -->
   <a class="primarycolor" v-if="false">
-    <Icon name="sent" color="blue" />
+    <Icon name="sent" color="blue"  class="toast-icon"/>
     Sending email...
     <ButtonPrimary>Undo</ButtonPrimary>
   </a>
 
+    <!-- TOAST Email Sent -->
+  <a class="primarycolor" v-if="true">
+    <Icon name="check" color="blue"  class="toast-icon"/>
+    Email sent
+  </a>
+
   <!-- TOAST Invite Sent -->
   <a class="primarycolor" v-if="false">
-    <Icon name="invite" color="blue" />
+    <Icon name="invite" color="blue"  class="toast-icon"/>
     Sending invite...
     <ButtonPrimary>Undo</ButtonPrimary>
   </a>
 
   <!-- TOAST Connection Lost -->
   <a class="red" v-if="false">
-    <Icon name="no-internet" color="red" />
+    <Icon name="no-internet" color="red"  class="toast-icon"/>
     We're having trouble connecting to the internet.
   </a>
 
   <!-- TOAST IMAP Error -->
   <a class="red" v-if="false">
-    <Icon name="no-internet" color="red" />
+    <Icon name="no-internet" color="red"  class="toast-icon"/>
     We're having issues connecting to your mail provider.
   </a>
 
   <!-- TOAST Send Email Error -->
   <a class="red" v-if="false">
-    <Icon name="no-internet" color="red" />
+    <Icon name="no-internet" color="red"  class="toast-icon"/>
     We were unable to send your message due to network issues
   </a>
 
   <!-- TOAST Board Creation Suggestion -->
-  <a class="bodycolor" v-if="true">
-    <Icon name="board" color="normal" />
+  <a class="bodycolor" v-if="false">
+    <Icon name="board" color="normal" class="toast-icon"/>
     Looks like you receive a lot of Travel emails.
     <ButtonPrimary>Create Travel board</ButtonPrimary>
   </a>
@@ -59,5 +65,9 @@ a {
   font-weight: 500;
   color: var(--primary-font-color);
   white-space: nowrap;
+}
+.toast-icon{
+      height: 17px;
+    margin-top: -3px;
 }
 </style>
