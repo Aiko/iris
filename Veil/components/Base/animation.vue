a2a183df71eb3e7fd374fb41ee6cba48d9cae3ae
Ruben Touitou <rubencharlestouitou@gmail.com>
2022-11-28 15:28:06 +0200

    idk

--- Veil/components/Base/animation.vue
+++ Veil/components/Base/animation.vue
@@ -1,41 +1,36 @@
 <script lang="ts" setup>
-import CheckAnimation from "@Veil/assets/animations/check.json";
-import FailureAnimation from "@Veil/assets/animations/failure.json";
-import LoadingAnimation from "@Veil/assets/animations/loading.json";
-import NotificationAnimation from "@Veil/assets/animations/notification.json";
-import PowerAnimation from "@Veil/assets/animations/power.json";
-import SettingsAnimation from "@Veil/assets/animations/settings.json";
-import WalletAnimation from "@Veil/assets/animations/wallet.json";
-import { computed } from "@vue/reactivity";
+import CheckAnimation from '@Veil/assets/animations/check.json'
+import FailureAnimation from '@Veil/assets/animations/failure.json'
+import LoadingAnimation from '@Veil/assets/animations/loading.json'
+import NotificationAnimation from '@Veil/assets/animations/notification.json'
+import PowerAnimation from '@Veil/assets/animations/power.json'
+import SettingsAnimation from '@Veil/assets/animations/settings.json'
+import WalletAnimation from '@Veil/assets/animations/wallet.json'
+import { computed } from "@vue/reactivity"
 
 const animations = {
-  check: CheckAnimation,
-  failure: FailureAnimation,
-  loading: LoadingAnimation,
-  notification: NotificationAnimation,
-  power: PowerAnimation,
-  settings: SettingsAnimation,
-  wallet: WalletAnimation,
-};
+  "check": CheckAnimation,
+  "failure": FailureAnimation,
+  "loading": LoadingAnimation,
+  "notification": NotificationAnimation,
+  "power": PowerAnimation,
+  "settings": SettingsAnimation,
+  "wallet": WalletAnimation,
+}
 
 const props = defineProps<{
   name?: string;
-  loop?: boolean;
-}>();
+  loop?: boolean
+}>()
 
 const animation = computed(() => {
-  return props.name ? animations[props.name] : {};
-});
+  // @ts-ignore
+  return (props.name) ? animations[props.name] : {}
+})
 </script>
 
 <template>
-  <lottie-animation
-    :animationData="animation"
-    :loop="loop ?? true"
-    :autoPlay="true"
-    :speed="1"
-    class="icon"
-  />
+  <lottie-animation :animationData="animation" :loop="loop ?? true" :autoPlay="true" :speed="1" class="icon" />
 </template>
 
 <style scoped>
--- Veil/components/Header/Toast.vue
+++ Veil/components/Header/Toast.vue
@@ -9,46 +9,46 @@ import Animation from "@Veil/components/Base/Animation.vue";
 
   <!-- TOAST Sending Email -->
   <a class="primarycolor" v-if="false">
-    <Icon name="sent" color="blue"  class="toast-icon"/>
+    <Icon name="sent" color="blue" class="toast-icon" />
     Sending email...
     <ButtonPrimary>Undo</ButtonPrimary>
   </a>
 
-    <!-- TOAST Email Sent -->
+  <!-- TOAST Email Sent -->
   <a class="primarycolor" v-if="true">
-    <Icon name="check" color="blue"  class="toast-icon"/>
+    <Icon name="check" color="blue" class="toast-icon" />
     <Animation name="power" class="power" />
     Email sent
   </a>
 
   <!-- TOAST Invite Sent -->
   <a class="primarycolor" v-if="false">
-    <Icon name="invite" color="blue"  class="toast-icon"/>
+    <Icon name="invite" color="blue" class="toast-icon" />
     Sending invite...
     <ButtonPrimary>Undo</ButtonPrimary>
   </a>
 
   <!-- TOAST Connection Lost -->
   <a class="red" v-if="false">
-    <Icon name="no-internet" color="red"  class="toast-icon"/>
+    <Icon name="no-internet" color="red" class="toast-icon" />
     We're having trouble connecting to the internet.
   </a>
 
   <!-- TOAST IMAP Error -->
   <a class="red" v-if="false">
-    <Icon name="no-internet" color="red"  class="toast-icon"/>
+    <Icon name="no-internet" color="red" class="toast-icon" />
     We're having issues connecting to your mail provider.
   </a>
 
   <!-- TOAST Send Email Error -->
   <a class="red" v-if="false">
-    <Icon name="no-internet" color="red"  class="toast-icon"/>
+    <Icon name="no-internet" color="red" class="toast-icon" />
     We were unable to send your message due to network issues
   </a>
 
   <!-- TOAST Board Creation Suggestion -->
   <a class="bodycolor" v-if="false">
-    <Icon name="board" color="normal" class="toast-icon"/>
+    <Icon name="board" color="normal" class="toast-icon" />
     Looks like you receive a lot of Travel emails.
     <ButtonPrimary>Create Travel board</ButtonPrimary>
   </a>
@@ -67,8 +67,9 @@ a {
   color: var(--primary-font-color);
   white-space: nowrap;
 }
-.toast-icon{
-      height: 17px;
-    margin-top: -3px;
+
+.toast-icon {
+  height: 17px;
+  margin-top: -3px;
 }
 </style>
