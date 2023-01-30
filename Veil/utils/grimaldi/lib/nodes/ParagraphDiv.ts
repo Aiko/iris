22644457e11ff16b9e6916007651122dc6034077
💔 <shah@priansh.com>
2023-01-30 02:02:09 -0500

    tabs

--- Veil/utils/grimaldi/lib/nodes/ParagraphDiv.ts
+++ Veil/utils/grimaldi/lib/nodes/ParagraphDiv.ts
@@ -1,57 +1,70 @@
 import { Node, mergeAttributes } from '@tiptap/core'
 
 export interface ParagraphOptions {
-  HTMLAttributes: Record<string, any>,
+	HTMLAttributes: Record<string, any>,
 }
 
 declare module '@tiptap/core' {
-  interface Commands<ReturnType> {
-    paragraphDiv: {
-      /**
-       * Toggle a div-style paragraph
-       */
-      setParagraphDiv: () => ReturnType,
-    }
-  }
+	interface Commands<ReturnType> {
+		paragraphDiv: {
+			/**
+			 * Toggle a div-style paragraph
+			 */
+			setParagraphDiv: () => ReturnType,
+		}
+	}
 }
 
 /** Like the default Paragraph Node, but with div */
 export const ParagraphDiv = Node.create<ParagraphOptions>({
-  name: 'paragraphdiv',
+	name: 'paragraphdiv',
 
-  priority: 1000,
+	priority: 1000,
 
-  defaultOptions: {
-    HTMLAttributes: {
-      style: 'display: block;'
-    },
-  },
+	defaultOptions: {
+		HTMLAttributes: {
+			style: 'display: block;'
+		},
+	},
 
-  group: 'block',
-  draggable: false,
-  content: '(paragraph|paragraphdiv)+',
+	group: 'block',
+	draggable: false,
+	content: '(paragraph|paragraphdiv)+',
 
-  parseHTML() {
-    return [
-      { tag: 'div' }
-    ]
-  },
+	parseHTML() {
+		return [
+			{ tag: 'div' }
+		]
+	},
 
-  renderHTML({ HTMLAttributes }) {
-    return ['div', mergeAttributes(this.options.HTMLAttributes, HTMLAttributes), 0]
-  },
+	renderHTML({ HTMLAttributes }) {
+		return ['div', mergeAttributes(this.options.HTMLAttributes, HTMLAttributes), 0]
+	},
 
-  addCommands() {
-    return {
-      setParagraphDiv: () => ({ commands }) => {
-        return commands.setNode('paragraphdiv')
-      },
-    }
-  },
+	addCommands() {
+		return {
+			setParagraphDiv: () => ({ commands }) => {
+				return commands.setNode('paragraphdiv')
+			},
+		}
+	},
 
-  addKeyboardShortcuts() {
-    return {
-      'Mod-Alt-0': () => this.editor.commands.setParagraphDiv(),
-    }
-  },
+	addKeyboardShortcuts() {
+		return {
+			'Mod-Alt-0': () => this.editor.commands.setParagraphDiv(),
+			'Tab': () => {
+				this.editor.chain()
+					.sinkListItem('listItem')
+					.command(({ tr }) => {
+						tr.insertText('\t')
+
+						return true
+					})
+					.run()
+
+				return true // <- make sure to return true to prevent the tab from blurring.
+			},
+		}
+	},
+
 })
