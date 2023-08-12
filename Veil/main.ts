d6d7ceffe0373ba6442ab60c0cf8108678a1407f
💔 <shah@priansh.com>
2023-08-12 17:50:27 -0400

    use new router

--- Veil/main.ts
+++ Veil/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from '@vue/runtime-dom'
 import App from '@Veil/App.vue'
-import router from '@Veil/router/index'
+import router from '@Veil/router'
 import LottieAnimation from "lottie-web-vue";
 import '@Veil/assets/css/base.css'
 
