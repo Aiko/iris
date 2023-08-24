8e2725137777202d616ce308c0d8c02b1eb8d8be
rubentouitou <rubencharlestouitou@gmail.com>
2023-08-24 10:25:38 -0400

    testing shadow

--- Veil/components/Filters/Filter.vue
+++ Veil/components/Filters/Filter.vue
@@ -9,6 +9,9 @@
 
 <style lang="scss" scoped>
 .filter {
-  margin-bottom: 30px;
+  margin-bottom: 25px;
+  box-shadow: inset 0px 0px 11px 0px #00000059;
+  padding: 5px 10px 10px 10px;
+  border-radius: var(--primary-border-radius);
 }
 </style>
